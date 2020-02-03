c313e9f33835821a227ff0f393b756b348dbcc43
yazgoo <yazgoo@gmail.com>
2020-02-03 23:22:06 +0100

    :art: add bin.rs

--- README.md
+++ README.md
@@ -2,7 +2,23 @@
 
 video players in the terminal with blockish over libcaca with LD_PRELOAD magic
 
-# using it
+# building it
+
+```shell
+$ cargo build --release
+```
+
+# using it (rust)
+
+```shell
+$ cargo run mplayer path/to/your/video
+```
+
+```shell
+$ cargo run mpv path/to/your/video
+```
+
+# using it (shell)
 
 ```shell
 $ ./mplayer path/to/your/video
--- src/bin.rs
+++ src/bin.rs
@@ -1,3 +1,28 @@
-pub fn main() {
-    println!("TODO")
+extern crate crossterm;
+use std::process::Command;
+use std::os::unix::process::CommandExt;
+use std::error::Error;
+use std::env;
+
+fn main() -> Result<(), Box<dyn Error>> {
+    let args: Vec<String> = env::args().collect();
+    let player = &args[1];
+    let path = &args[2];
+    let (width, height) = crossterm::terminal::size()?;
+    let width = width * 8;
+    let height = height * 16 - 1;
+   Command::new(player)
+       .env("CACA_DRIVER", "raw")
+       .env("LD_PRELOAD", "target/release/libcaca_blockish.so")
+       .env("BLOCKISH_WIDTH", width.to_string())
+       .env("BLOCKISH_HEIGHT", height.to_string())
+       .arg("-quiet")
+       .arg("-vo")
+       .arg("caca")
+       .arg("-vf")
+       .arg(format!("scale={}:{}", &width, &height))
+       .arg(path)
+       .exec();
+
+    Ok(())
 }
