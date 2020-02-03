e2cd7ba8d04921036566c1b240604ebff0f5ac2e
yazgoo <yazgoo@gmail.com>
2020-02-03 10:19:47 +0100

    :bug: relative links

--- setup.sh
+++ setup.sh
@@ -1,9 +1,11 @@
 set -xe
-cargo build --release
+SCRIPT=$(readlink -f "$0")
+SCRIPTPATH=$(dirname "$SCRIPT")
+(cd $SCRIPTPATH && cargo build --release)
 # use raw mode instead of canvas mode (so that there's no black foreground)
 export CACA_DRIVER=raw
 # replace libcaca functions
-export LD_PRELOAD=target/release/libcaca_blockish.so
+export LD_PRELOAD="${SCRIPTPATH}/"target/release/libcaca_blockish.so
 # pass video height width to libcaca_blockish
 export BLOCKISH_WIDTH=$(( `tput cols` * 8 ))
 export BLOCKISH_HEIGHT=$(( `tput lines` * 16 - 1 ))
