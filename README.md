c90e422d41f41a4abea9f7965f50be64b464b6b4
yazgoo <yazgoo@gmail.com>
2020-02-06 19:11:00 +0100

    :recycle: refactoring + doc

--- README.md
+++ README.md
@@ -1,6 +1,6 @@
 # blockish-caca
 
-video players in the terminal with blockish over libcaca with LD_PRELOAD magic
+video players in the terminal with [blockish](https://github.com/yazgoo/blockish) (utf-8 blocks) over libcaca with LD_PRELOAD magic
 
 [![Alt text](https://img.youtube.com/vi/HminM4V40iI/0.jpg)](https://www.youtube.com/watch?v=HminM4V40iI)
 
--- src/bin.rs
+++ src/bin.rs
@@ -3,30 +3,31 @@ use std::process::Command;
 use std::os::unix::process::CommandExt;
 use std::error::Error;
 use std::env;
-use std::path::Path;
+
+fn play_video(player: &String, path: &String) -> Result<(), Box<dyn Error>> {
+    let (width, height) = crossterm::terminal::size()?;
+    let width = width * 8;
+    let height = height * 16 - 1;
+    let exe = env::current_exe()?;
+    let bin_dir = exe.parent().ok_or("bin_dir")?.to_str().ok_or("to str")?;
+    Command::new(player)
+        .env("CACA_DRIVER", "raw")
+        .env("LD_PRELOAD", format!("{}/libcaca_blockish.so", bin_dir))
+        .env("BLOCKISH_WIDTH", width.to_string())
+        .env("BLOCKISH_HEIGHT", height.to_string())
+        .arg("-quiet")
+        .arg("-vo")
+        .arg("caca")
+        .arg("-vf")
+        .arg(format!("scale={}:{}", &width, &height))
+        .arg(path)
+        .exec();
+    Ok(())
+}
 
 fn main() -> Result<(), Box<dyn Error>> {
     let args: Vec<String> = env::args().collect();
     let player = &args[1];
     let path = &args[2];
-    let (width, height) = crossterm::terminal::size()?;
-    let width = width * 8;
-    let height = height * 16 - 1;
-    let exe = env::current_exe()?;
-    let exe_path = Path::new(&exe.to_str().ok_or("to str")?);
-    let bin_dir = exe.parent().ok_or("bin_dir")?.to_str().ok_or("to str")?;
-   Command::new(player)
-       .env("CACA_DRIVER", "raw")
-       .env("LD_PRELOAD", format!("{}/libcaca_blockish.so", bin_dir))
-       .env("BLOCKISH_WIDTH", width.to_string())
-       .env("BLOCKISH_HEIGHT", height.to_string())
-       .arg("-quiet")
-       .arg("-vo")
-       .arg("caca")
-       .arg("-vf")
-       .arg(format!("scale={}:{}", &width, &height))
-       .arg(path)
-       .exec();
-
-    Ok(())
+    play_video(player, path)
 }
